5d0d9ff114ed36b1907126e6a5df328dda5bacd6
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-09-05 00:54:58 +0000

    admin_edit_finish

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  
+  resources :genres, only: [:index, :create, :edit, :update]
 end
