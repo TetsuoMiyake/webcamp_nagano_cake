bf52ad39e4262a5cb49f8d258dfc92c9660a6772
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-09-05 22:40:44 +0000

    admin_item_detail[start]

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,7 @@ class ItemsController < ApplicationController
   end
   
   def show
-    
+    @item = Item.find(params[:id])
   end
   
   def edit
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2ffca35ecf46e810baeecb0d0ee6264c229c993ded8be5ef2c6edd58c23437e4e9d02b15ba2e081b59b22599f684e63b8884d77603566affb2bc7aba681bd5f8'
