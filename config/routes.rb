5c8311a0ed4a64a7581d59e05a9b9bdb04e8e2d0
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-09-06 13:16:20 +0000

    customer_table[finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+    devise_for :admins, controllers: {
+    sessions: 'devise/sessions',
+    passwords: 'devise/passwords',
+    registrations: 'devise/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   
   resources :genres, only: [:index, :create, :edit, :update]
   
   resources :items, only: [:new, :index, :create, :show, :edit, :update]
+  
+  resources :admins
+  resources :customers, only: [:index]
 end
