7c2d410fd474ea0471509f882e10c5bbcba49ce3
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-09-06 22:47:45 +0000

    admin_customer_index[finish]

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,13 @@ class CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+  
+  def show
+    @customers.find(params[:id])
+  end
+  
+  def item_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
+  end
+  
 end
