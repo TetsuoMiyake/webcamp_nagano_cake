00c7beb3855e30256591ffd10832a1fc72f7ecde
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-09-08 22:27:07 +0000

    admin_items[finish]

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,31 +1,11 @@
 class ItemsController < ApplicationController
   
-  def new
-    @item = Item.new
-  end
-  
-  def create
-    @item = Item.new(item_params)
-    @item.save
-    redirect_to items_path
-  end
-  
   def index
     @items = Item.all
   end
   
   def show
     @item = Item.find(params[:id])
-  end
-  
-  def edit
-    @item = Item.find(params[:id])
-  end
-  
-  def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    redirect_to items_path
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,12 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   
   resources :genres, only: [:index, :create, :edit, :update]
-  
-  resources :items, only: [:new, :index, :create, :show, :edit, :update]
-  
+  resources :items, only: [:index, :show]
   resources :admins
   resources :customers, only: [:index, :show, :edit, :update]
+  resources :order_details, only: [:index, :show, :edit, :update]
+  
+  namespace :admin do
+      resources :items, only: [:new, :index, :create, :show, :edit, :update]
+  end
 end
