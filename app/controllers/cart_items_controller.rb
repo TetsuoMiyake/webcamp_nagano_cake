e45ca62827df04416678b1f3d34b6e0971594672
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-09-13 13:26:25 +0000

    cart_item_total_amount[start]

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,14 +1,33 @@
 class CartItemsController < ApplicationController
   
+  def index
+    cart_items = CartItem.all
+    @cart_items = current_customer.cart_items
+  end
+  
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
+  end
+  
   def create
     @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
     @cart_item.save
+    redirect_to cart_items_path
+  end
+  
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
     redirect_to cart_items_path
   end
   
   private
   
-  def genre_params
+  def cart_item_params
     params.require(:cart_item).permit(:amount, :customer_id, :item_id)
   end
+  
 end
